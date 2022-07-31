779f12de641dc5d5a90c27b80a60fe9950c4937b
Zchun <z.c.chun2003@gmail.com>
2022-07-31 14:19:14 -0500

    category post route created

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -34,15 +34,21 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = Category.create({category_name: req.body.category_name});
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
 });
 
