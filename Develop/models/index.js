059b70700d803a9b8a80de1194355ba3cd5a142d
Zchun <z.c.chun2003@gmail.com>
2022-07-31 14:41:04 -0500

    category routes

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,14 +4,16 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id'
+  foreignKey: 'category_id',
+  onUpdate: 'cascade'
 });
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE'
+  onUpdate: 'cascade'
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
